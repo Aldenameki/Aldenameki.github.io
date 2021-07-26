a8c2f1c6479ca9367d2a45b560f7027a72802f08
Aldenameki <43084684+Aldenameki@users.noreply.github.com>
2021-07-26 05:00:49 -0300

    Add files via upload

--- website/check/logic.js
+++ website/check/logic.js
@@ -14,12 +14,18 @@ function start() {
     
 }
 
-function createauth() {
-    let userIdBuffer = new Uint8Array(16);
+function challenge() {
     let challengeBuffer = new Uint8Array([
         0x8C, 0x0A, 0x26, 0xFF, 0x22, 0x91, 0xC1, 0xE9, 0xB9, 0x4E, 0x2E, 0x17, 0x1A, 0x98, 0x6A, 0x73,
         0x71, 0x9D, 0x43, 0x48, 0xD5, 0xA7, 0x6A, 0x15, 0x7E, 0x38, 0x94, 0x52, 0x77, 0x97, 0x0F, 0xEF
     ]).buffer;
+
+    return challengeBuffer;
+}
+
+function createauth() {
+    let userIdBuffer = new Uint8Array(16);
+    let challengeBuffer = challenge();
 
     let options = {
         publicKey: {
@@ -48,6 +54,8 @@ function createauth() {
 }
 
 function loginauth() {
+    let challengeBuffer = challenge();
+
     let options = {
         publicKey: {
             challenge: challengeBuffer,
